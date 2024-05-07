019c900215de5087f739d2438711a54d45c40749
y-mitsuyoshi <glorymanchesteru@gmail.com>
2024-05-08 08:40:24 +0900

    add rss library

--- main.go
+++ main.go
@@ -4,9 +4,29 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/mmcdole/gofeed"
 )
 
 func main() {
+	feed, err := gofeed.NewParser().ParseURL("https://zenn.dev/spiegel/feed")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		return
+	}
+	fmt.Println(feed.Title)
+	fmt.Println(feed.FeedType, feed.FeedVersion)
+	for _, item := range feed.Items {
+		if item == nil {
+			break
+		}
+		fmt.Println(item.Title)
+		fmt.Println("\t->", item.Link)
+		fmt.Println("\t->", item.PublishedParsed.Format(time.RFC3339))
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, World!")
 	})
